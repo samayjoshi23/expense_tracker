d3a2b30f0af65648206d498fef4b16051971c116
Samay Joshi <63327946+samayjoshi23@users.noreply.github.com>
2022-04-25 22:16:34 +0530

    First commit of the application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from "react-bootstrap/Container";
+import { Stack, Button } from "react-bootstrap";
+import BudgetCard from "./Components/BudgetCard";
+import AddBudgetModal from "./Components/AddBudgetModal";
+import { useState } from "react";
 
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>Add Budget</Button>
+        <Button variant="primary">Add Expense</Button>
+      </Stack>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill,minmax(300px,1fr))",
+          gap: "1rem",
+          alignItems: "flex-start",
+        }}
+      >
+        <BudgetCard name="Entertainment" amount={1800} max={2000} />
+      </div>
+    </Container>
+    <AddBudgetModal show={showAddBudgetModal} handleClose={() => setShowAddBudgetModal(false)}/>
+  </>
   );
 }
 
---
+++ src/Utils/utils.js
@@ -0,0 +1,5 @@
+export const currenyFormatter = new Intl.NumberFormat(undefined, {
+    currency: "inr",
+    style: "currency",
+    minimumFractionDigits: 0
+})
